c72d4997202fbc0611127569e04f78174000fd3c
Zach Heckert <zach@zachheckert.com>
2020-08-26 16:57:42 -0600

    Creating a repository for personal practice projects

---
+++ Junk Practice/Practice.js
@@ -0,0 +1,25 @@
+//1
+// var peopleArray = [
+//     {
+//       name: "Harrison Ford",
+//       occupation: "Actor"
+//     },
+//     {
+//       name: "Justin Bieber",
+//       occupation: "Singer"
+//     },
+//     {
+//       name: "Vladimir Putin",
+//       occupation: "Politician"
+//     },
+//     {
+//       name: "Oprah",
+//       occupation: "Entertainer"
+//     }
+//   ]
+
+//   for (let i = 0; i < peopleArray.length; i++) {
+//       console.log(peopleArray[i].name);
+//   }
+
+
---
+++ Legendary Weapon Affix/legendaryWeaponAffix.js
@@ -0,0 +1,29 @@
+// 1. Choose one
+    let weaponPrefix = [
+        ["crushing", "smashing", "blunt", "flat", "ultradense", "weighted", "studded"]
+        ["vorpal", "scintillating", "honed", "cutting", "slicing", "chopping", "sharp", "rending", "severing", "sharpened"]
+    ]
+
+// 2. Choose one
+
+    let weaponAdjective = ["filth-encrusted", "dirty", "gem-encrusted", "gilded", "golden", "vomit-coated", "bile-coated", "hallowed", "gore-stained", "hell-spawned", "blood-soaked", "super-heated","conflagrating", "engraved", "carved", "magic", "enhanced", "refined", "envenomed", "venomous", "soul-stealing", "regenerating", "enchanted", "ensorceled", "legendary", "rune-inscribed", ]
+
+    let weaponType = [
+        ["longsword", "broadsword", "falchion", "zweihander", "short sword", "claymore", "greatsword", "dagger", "kris", "handaxe", "greataxe"]
+        ["warhammer", "mallet", "club", "basher", "flail", "mace", "cane", "knuckleduster", "morningstar", "cudgel", "quarterstaff"]
+    ]
+
+// 4. Choose one
+    // this will need to have "of" in front of it.
+    let suffix = of ["darkness", "light", "flame", "heat", "explosions", "flames", "ice", "hatred", "puissance", "malevolence", "potency", "power", "truth", "might", "carnage", "legend", "trauma", "rage", "fury", "insanity", "madness", "divine", "relic", ]
+        // rare suffix? of = ["caliginous funerals", "eternal moonlight", "ceaseless bloodthirst", "holy requiems", "unbridled magnificence", ]
+
+function generate(){
+    let part1 = Math.random()
+    let part2 =
+    let part3 = 
+}
+
+console.log(weapon)
+
+// Talk to sabelyn about randomly generating array stuff
