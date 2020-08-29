25a78914dade390ea8b05b603f006ac9ab9d9115
Zach Heckert <zach@zachheckert.com>
2020-08-29 10:41:26 -0600

    Added a few more affixes, etc.

--- Legendary Weapon Affix/legendaryWeaponAffix.js
+++ Legendary Weapon Affix/legendaryWeaponAffix.js
@@ -1,12 +1,12 @@
-const blunt = ["crushing ", "smashing ", "blunt ", "ultradense ", "weighted ", "studded ",]
+const blunt = ["crushing ", "smashing ", "blunt ", "ultradense ", "weighted ", "studded ", "heavy ",]
 const bladed = ["vorpal ", "scintillating ", "honed ", "cutting ", "slicing ", "chopping ", "rending ", "severing ", "sharpened ", "jagged ", "serrated ",]
 
 let weaponAdjective = ["filth-encrusted ", "fetid ", "dirty ", "gem-encrusted ", "gilded ", "golden ", "vomit-coated ", "bile-coated ", "hallowed ", "gore-stained ", "hell-spawned ", "blood-soaked ", "super-heated ","conflagrating ", "engraved ", "carved ", "magic ", "enhanced ", "refined ", "envenomed ", "venomous ", "soul-stealing ", "regenerating ", "enchanted ", "ensorceled ", "legendary ", "rune-etched ", "heroic ", "blessed ",]
 
-const bladedType = ["longsword ", "broadsword ", "falchion ", "zweihander ", "short sword ", "claymore ", "greatsword ", "dagger ", "kris ", "handaxe ", "greataxe ", "glaive ", "halberd ", "scimitar ", ]
+const bladedType = ["longsword ", "broadsword ", "falchion ", "zweihander ", "short sword ", "claymore ", "greatsword ", "dagger ", "butcher's knife ", "kris ", "handaxe ", "greataxe ", "glaive ", "halberd ", "scimitar ", ]
 const bluntType = ["warhammer ", "mallet ", "club ", "maul ", "flail ", "mace ", "cane ", "knuckleduster ", "morningstar ", "cudgel ", "quarterstaff ", "sledgehammer", "stave ",]
 
-let suffix = ["darkness ", "light ", "flame ", "heat ", "explosions ", "thunderbolts ", "flames ", "ice ", "hatred ", "puissance ", "malevolence ", "potency ", "power ", "truth ", "might ", "carnage ", "legend ", "trauma ", "rage ", "fury ", "insanity ", "madness ", "divinity ", "caliginous funerals ", "eternal moonlight ", "ceaseless bloodthirst ", "holy requiems ", "unbridled magnificence ", "decrepit coffins ", "dessicated corpses ", "angelic radiance ","malignant cognizance ", "leprous convergence ", "the fungal blossoms", ]
+let suffix = ["darkness ", "light ", "flame ", "heat ", "explosions ", "thunderbolts ", "flames ", "ice ", "hatred ", "puissance ", "malevolence ", "potency ", "power ", "truth ", "might ", "carnage ", "legend ", "trauma ", "rage ", "fury ", "insanity ", "madness ", "divinity ", "caliginous funerals ", "eternal moonlight ", "ceaseless bloodthirst ", "holy requiems ", "unbridled magnificence ", "decrepit coffins ", "dessicated corpses ", "angelic radiance ","malignant cognizance ", "leprous convergence ", "the fungal blossoms", "wretched putrescence ", ]
 
 function legendaryWeaponGenerator(){
     let style = Math.floor(Math.random() * 2)
