3df7851c17bf64513fd53a30ec6f393e0d802bc6
Zach Heckert <zach@zachheckert.com>
2020-08-28 18:17:44 -0600

    Completed weapon generator logic and added/removed some prefix/suffixes. Now I just need to add many more!

--- Legendary Weapon Affix/legendaryWeaponAffix.js
+++ Legendary Weapon Affix/legendaryWeaponAffix.js
@@ -1,29 +1,22 @@
-// 1. Choose one
-    let weaponPrefix = [
-        ["crushing", "smashing", "blunt", "flat", "ultradense", "weighted", "studded"]
-        ["vorpal", "scintillating", "honed", "cutting", "slicing", "chopping", "sharp", "rending", "severing", "sharpened"]
-    ]
+const blunt = ["crushing ", "smashing ", "blunt ", "ultradense ", "weighted ", "studded ",]
+const bladed = ["vorpal ", "scintillating ", "honed ", "cutting ", "slicing ", "chopping ", "rending ", "severing ", "sharpened ", "jagged ", "serrated ",]
 
-// 2. Choose one
+let weaponAdjective = ["filth-encrusted ", "fetid ", "dirty ", "gem-encrusted ", "gilded ", "golden ", "vomit-coated ", "bile-coated ", "hallowed ", "gore-stained ", "hell-spawned ", "blood-soaked ", "super-heated ","conflagrating ", "engraved ", "carved ", "magic ", "enhanced ", "refined ", "envenomed ", "venomous ", "soul-stealing ", "regenerating ", "enchanted ", "ensorceled ", "legendary ", "rune-etched ", "heroic ", "blessed ",]
 
-    let weaponAdjective = ["filth-encrusted", "dirty", "gem-encrusted", "gilded", "golden", "vomit-coated", "bile-coated", "hallowed", "gore-stained", "hell-spawned", "blood-soaked", "super-heated","conflagrating", "engraved", "carved", "magic", "enhanced", "refined", "envenomed", "venomous", "soul-stealing", "regenerating", "enchanted", "ensorceled", "legendary", "rune-inscribed", ]
+const bladedType = ["longsword ", "broadsword ", "falchion ", "zweihander ", "short sword ", "claymore ", "greatsword ", "dagger ", "kris ", "handaxe ", "greataxe ", "glaive ", "halberd ", "scimitar ", ]
+const bluntType = ["warhammer ", "mallet ", "club ", "maul ", "flail ", "mace ", "cane ", "knuckleduster ", "morningstar ", "cudgel ", "quarterstaff ", "sledgehammer", "stave ",]
 
-    let weaponType = [
-        ["longsword", "broadsword", "falchion", "zweihander", "short sword", "claymore", "greatsword", "dagger", "kris", "handaxe", "greataxe"]
-        ["warhammer", "mallet", "club", "basher", "flail", "mace", "cane", "knuckleduster", "morningstar", "cudgel", "quarterstaff"]
-    ]
+let suffix = ["darkness ", "light ", "flame ", "heat ", "explosions ", "thunderbolts ", "flames ", "ice ", "hatred ", "puissance ", "malevolence ", "potency ", "power ", "truth ", "might ", "carnage ", "legend ", "trauma ", "rage ", "fury ", "insanity ", "madness ", "divinity ", "caliginous funerals ", "eternal moonlight ", "ceaseless bloodthirst ", "holy requiems ", "unbridled magnificence ", "decrepit coffins ", "dessicated corpses ", "angelic radiance ","malignant cognizance ", "leprous convergence ", "the fungal blossoms", ]
 
-// 4. Choose one
-    // this will need to have "of" in front of it.
-    let suffix = of ["darkness", "light", "flame", "heat", "explosions", "flames", "ice", "hatred", "puissance", "malevolence", "potency", "power", "truth", "might", "carnage", "legend", "trauma", "rage", "fury", "insanity", "madness", "divine", "relic", ]
-        // rare suffix? of = ["caliginous funerals", "eternal moonlight", "ceaseless bloodthirst", "holy requiems", "unbridled magnificence", ]
-
-function generate(){
-    let part1 = Math.random()
-    let part2 =
-    let part3 = 
+function legendaryWeaponGenerator(){
+    let style = Math.floor(Math.random() * 2)
+    if(style === 0){
+        const bladedWeapon = bladed[Math.floor(Math.random()*blunt.length)] + weaponAdjective[Math.floor(Math.random()*weaponAdjective.length)] + bladedType[Math.floor(Math.random()*bluntType.length)] + "of " + suffix[Math.floor(Math.random()*suffix.length)];
+        return bladedWeapon;
+    } else if(style === 1){
+        const bluntWeapon = blunt[Math.floor(Math.random()*blunt.length)] + weaponAdjective[Math.floor(Math.random()*weaponAdjective.length)] + bluntType[Math.floor(Math.random()*bluntType.length)] + "of " + suffix[Math.floor(Math.random()*suffix.length)];
+        return bluntWeapon;
+    }
 }
 
-console.log(weapon)
-
-// Talk to sabelyn about randomly generating array stuff
+console.log(legendaryWeaponGenerator());
