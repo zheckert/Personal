6f390fa375605d9ebbaac5d4a699887b0e7358c5
Zach Heckert <zach@zachheckert.com>
2020-10-28 11:07:45 -0600

    Redefined a valid move so you can't move a square anywhere. Fixed squares not removing when 3 or 4 are matched. Listed some ideas for future changes/improvements. Credited source.

--- match-3/match-3.js
+++ match-3/match-3.js
@@ -1,9 +1,15 @@
-//How would we turn this into something like that one PSP game that was a matcher? We could instantiate classes and have health and armor values and formulas to calculate damage. 
+//Made utilizing https://www.youtube.com/watch?v=XD5sZWxwJUk&feature=youtu.be as a base. 
+
+//How would we turn this into something like that one PSP game that was a matcher? We could instantiate classes and have health and armor values and formulas to calculate damage.  Also add cool graphics
+
+//to debug: matching 3 doesn't remove jack (could have something to do with "undefined square error in the console"), and you can drag a square anywhere, meaning there currently aren't invalid moves, 
+//function(s) for removing blocks should be called after a move takes place, then remove setInterval function (Wouldn't this go at the end of a move, so check for matches at dragEnd? AND check for 4 before 3. Maybe we need to have it so a row of ANY length can be removed, with a more complicated score modifier?)
 
 document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector(".grid")
     const width = 8
     const squares = []
+    let score = 0
 
     const widgetColors = [
         "F531B1",
@@ -46,27 +52,6 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(this.id, "dragStart")
     }
 
-    function dragEnd() {
-        console.log(this.id, "dragEnd")
-        let validMoves = [
-            squareIdBeingDragged -1, 
-            squareIdBeingDragged +1, 
-            squareIdBeingDragged -width,
-            squareIdBeingDragged +width 
-        ]
-
-        let validMove = validMoves.includes(squareIdBeingReplaced)
-
-        if(squreIdBeingReplaced && validMove) {
-            squareIdBeingReplaced = null
-        } else if(squareIdBeingReplaced && !validMove) {
-            squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced
-            squares[squareIdBeingReplaced].style.backgroundColor = colorBeingDragged
-        }else{
-            squares[squareIdBeingDragged.style.backgroundColor = colorBeingDragged]
-        }
-    }
-
     function dragOver(e) {
         e.preventDefault()
         console.log(this.id, "dragOver")
@@ -89,4 +74,103 @@ document.addEventListener("DOMContentLoaded", () => {
         squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced
     }
 
+    function dragEnd() {
+        console.log(this.id, "dragEnd")
+        let validMoves = [
+            squareIdBeingDragged -1, 
+            squareIdBeingDragged -width,
+            squareIdBeingDragged +1, 
+            squareIdBeingDragged +width 
+        ]
+
+        let validMove = validMoves.includes(squareIdBeingReplaced)
+
+        if(squareIdBeingReplaced && validMove) {
+            squareIdBeingReplaced = null
+        } else if (squareIdBeingReplaced && !validMove) {
+            squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced
+            squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+        } else squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+    }
+
+    function checkRowForThree() {
+        for (i = 0; i < 61; i++) {
+            let rowOfThree = [i, i+1, i+2]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ""
+            
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+            if (notValid.includes(i)) continue
+
+            if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 3
+                rowOfThree.forEach(index => {
+                squares[index].style.backgroundColor = ""
+                })
+            }
+        }
+    }
+    checkRowForThree()
+    
+    function checkColumnForThree() {
+        for (i = 0; i < 47; i++) {
+            let columnOfThree = [i, i+width, i+width*2]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ""
+    
+            if (columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 3
+                columnOfThree.forEach(index => {
+                squares[index].style.backgroundColor = ""
+                })
+            }
+    
+        }
+    }
+    checkColumnForThree()
+
+    function checkRowForFour() {
+        for (i = 0; i < 60; i++) {
+            let rowOfFour = [i, i+1, i+2, i+3]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ""
+            
+            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55]
+            if (notValid.includes(i)) continue
+
+            if (rowOfFour.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 5
+                rowOfFour.forEach(index => {
+                squares[index].style.backgroundColor = ""
+                })
+            }
+        }
+    }
+    checkRowForFour()
+    
+    function checkColumnForFour() {
+        for (i = 0; i < 47; i++) {
+            let columnOfFour = [i, i+width, i+width*2, i+width*3]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ""
+    
+            if (columnOfFour.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 5
+                columnOfFour.forEach(index => {
+                squares[index].style.backgroundColor = ""
+                })
+            }
+    
+        }
+    }
+    checkColumnForFour()
+    
+    window.setInterval(function(){
+        checkRowForFour()
+        checkColumnForFour()
+        checkRowForThree()
+        checkColumnForThree()
+    }, 100)
 })
+
+
