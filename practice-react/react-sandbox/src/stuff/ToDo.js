5b3218973153a5f41db989d6418347db910f2209
Zach Heckert <zach@zachheckert.com>
2020-11-12 12:11:58 -0700

    Added useRef() to form. It's done for now.

--- practice-react/react-sandbox/src/stuff/ToDo.js
+++ practice-react/react-sandbox/src/stuff/ToDo.js
@@ -1,19 +1,20 @@
-import React, {useState} from "react"
+import React, {useState, useRef} from "react"
 
 export const ToDo = () => {
 
     const [toDoItem, setToDoItem] = useState("")
     const [list, setList] = useState([])
+    const inputRef = useRef(null)
 
-    const addToDo = (event) =>{
-        event.preventDefault()
-        setList(prevList => [...prevList, setToDoItem])
-        setToDoItem("")
+    const handleChange = (event) => {
+        setToDoItem(event.target.value)
     }
 
-    const handleChange = (event) =>{
+    const addToDo = (event) => {
         event.preventDefault()
-        setToDoItem(event.target.value)
+        setList(prevList => [...prevList, toDoItem])
+        setToDoItem("")
+        inputRef.current.focus()
     }
 
     const allItems = list.map(toDo => <p key={toDo}>{toDo}</p>)
@@ -21,8 +22,15 @@ export const ToDo = () => {
     return(
         <div>
             <form>
-                <input type="text" placeholder="Type your To-do item here." value={toDoItem} onChange={handleChange}></input>
-                <button onClick={addToDo}>Click to add this to your list</button>
+                <input 
+                    ref={inputRef}
+                    type="text" 
+                    name="toDo" 
+                    placeholder="Type your To-do item here." 
+                    value={toDoItem} 
+                    onChange={handleChange}>
+                </input>
+                <button onClick={addToDo}>Add to list</button>
             </form>
             {allItems}
         </div>
